ac9345459a351337239cfb011890db104903fbc0
Marlliton souza <marllitonsouza@pop-os.localdomain>
2021-01-28 14:03:10 -0300

    Linter e State

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
+import React from 'react'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from '../db.json'
+import Header from 'next/head'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -12,7 +14,7 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
     font-family: sans-serif;
-    color: ${({theme}) => theme.colors.contrastText};
+    color: ${({ theme }) => theme.colors.contrastText};
   }
 
   html, body {
@@ -28,9 +30,17 @@ const GlobalStyle = createGlobalStyle`
 
 const theme = db.theme
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Header>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Header>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import QuizBackground from '../src/components/QuizBackground'
 import db from '../db.json'
 import Widget from '../src/components/Widgets'
 import Footer from '../src/components/Footer'
 import GithubCorner from '../src/components/GithubCorner'
+import QuizLogo from '../src/components/QuizLogo'
+import { useRouter } from 'next/router'
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -17,24 +20,45 @@ export const QuizContainer = styled.div`
 `
 
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = useState('')
+  console.log(name, setName)
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum dolor sit amet...</p>
+            <form
+              onSubmit={function (event) {
+                event.preventDefault()
+                router.push(`/quiz?name=${name}`)
+              }}
+            >
+              <input
+                placeholder="Diz ai seu nome"
+                onChange={event => {
+                  // const value = event.target.value
+                  // console.log(value)
+                  setName(event.target.value)
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
-
         <Widget>
           <p>lorem ipsum dolor sit amet...</p>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GithubCorner projectUrl="https://github.com/Marlliton/aluraquiz-base"/>
+      <GithubCorner projectUrl="https://github.com/Marlliton/aluraquiz-base" />
     </QuizBackground>
   )
 }
